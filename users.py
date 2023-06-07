187525908c0ab62738ffb1a8dcc25d886e492888
Brandon Colangelo <brandoncancode@gmail.com>
2023-06-07 11:37:46 -0400

    Finished TO DO list

--- users.py
+++ users.py
@@ -12,22 +12,22 @@ class User:
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('users_schema').query_db(query)
+        results = connectToMySQL('users2_schema').query_db(query)
         users = []
-        for user in results:
-            users.append(cls(user))
+        for u in results:
+            users.append(cls(u))
         return users
-    
+
     @classmethod
     def save(cls, data):
         query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);"
 
         #comes back as the new row id. If the query is bad it will come back as False
-        result = connectToMySQL('users_schema').query_db(query, data)
+        result = connectToMySQL('users2_schema').query_db(query, data)
         return result
 
 
 # ***** TO DO *******
-#Fix Header on Create new user page
-#Fix issue with not showing the created at
-#Add documentation
+#Fix Header on Create new user page(Done)
+#Fix issue with not showing the created at(Done)
+#Add documentation(Done)
